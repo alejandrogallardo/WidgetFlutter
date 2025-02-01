881271c7183bc9260421def29213ffdd39b6dfd5
Alejandro Gallardo <ridesnstuffgt@gmail.com>
2025-01-31 20:11:17 -0600

    Se crearon botones

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ButtonsScreen extends StatelessWidget {
   const ButtonsScreen({super.key});
@@ -9,7 +10,97 @@ class ButtonsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Botones Screen'),
       ),
-      body: const Placeholder(),
+      body: const _ButtonsView(),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.arrow_back_ios_new_rounded),
+        onPressed: () {
+          context.pop();
+        },
+      ),
+    );
+  }
+}
+
+class _ButtonsView extends StatelessWidget {
+  const _ButtonsView();
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return SizedBox(
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: Wrap(
+          spacing: 10,
+          alignment: WrapAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){}, child: const Text('Elevated Button')),
+            const ElevatedButton(onPressed: null, child: Text('Elevated Button Disabled')),
+            ElevatedButton.icon(
+              onPressed: (){}, 
+              icon: const Icon(Icons.access_alarm_rounded),
+              label: const Text('Elevated Icon'),
+            ),
+            FilledButton(
+              onPressed: (){}, 
+              child: const Text('Filled')
+            ),
+            FilledButton.icon(
+              onPressed: (){}, 
+              icon: const Icon(Icons.account_balance_outlined), 
+              label: const Text('Filled Icons')
+            ),
+            OutlinedButton(onPressed: (){}, child: const Text('Outline')),
+            OutlinedButton.icon(
+              onPressed: (){},
+              icon: const Icon(Icons.add_a_photo),
+              label: const Text('Outline Icon')
+            ),
+            TextButton(onPressed: (){}, child: const Text('Text')),
+            TextButton.icon(
+              onPressed: (){}, 
+              icon: const Icon(Icons.add_reaction_rounded), 
+              label: const Text('Icon Text')
+            ),
+            IconButton(
+              onPressed: (){}, 
+              icon: const Icon(Icons.ac_unit_sharp)
+            ),
+            IconButton(
+              onPressed: (){}, 
+              icon: const Icon(Icons.abc_sharp),
+              style: ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(colors.primary),
+                iconColor: const MaterialStatePropertyAll(Colors.white),
+              ),
+            ),
+            const CustomButton(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override  
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Material(
+        color: colors.primary,
+        child: InkWell(
+          onTap: (){},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text('Hola Perros', style: TextStyle(color: Colors.white),)
+          ),
+        ),
+      ),
     );
   }
 }
